5e4f5562ae1081a848cc8b068fff9de664152e5b
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-04-12 15:20:05 +0530

    Co-authored-by: Mayankk7 <Mayankk7@users.noreply.github.com>

--- app/server/app.py
+++ app/server/app.py
@@ -1,11 +1,22 @@
+###################################################################################################################################
+#importing module
+###################################################################################################################################
+
 from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 # from pymongo import MongoClient
 import motor.motor_asyncio
-from bson import ObjectId
-import json
-import os
+from passlib.context import CryptContext
+import jwt
+import datetime
+from pyresparser import ResumeParser
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
 
@@ -24,22 +35,66 @@ app.add_middleware(
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 db = client.jobfindr
 
-# schemas 
+#################################################################################################################################
+# SCHEMAS FOR MONGOOSE DATABASE
+#################################################################################################################################
 class User(BaseModel):
     id: str
     email: str
     password: str
     name: str
+class NewUser(BaseModel):
+    email: str
+    password: str
+    name: str
 
+class Organization(BaseModel):
+    email:str
+    password:str
+    name: str
+
+class Job(BaseModel):
+    description: str
+    skills : list
+    organization : str
+    contact_email : str
+
+
+#####################################################################################################################################
+# token generation 
+#####################################################################################################################################
+def signJwt(user):
+    payload = {
+        'id': user['id'],
+        'email': user['email'],
+        'exp': datetime.utcnow() + 600
+    }
+    return jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+
+
+#####################################################################################################################################
+#testing route
+#####################################################################################################################################
 @app.get('/')
 def index():
   return {'message' : 'Hello from server'}
 
+#####################################################################################################################################
 # user db functions 
+#####################################################################################################################################
+
 async def create_user(user):
     document = user
+    user_exists = await fetch_one_user(document['email'])
+    if user_exists:
+        raise HTTPException(status_code=400, detail="User already exists")
+    document['password'] = pwd_context.hash(document['password'])
     result = await db.users.insert_one(document)
+    document['id'] = str(document['_id'])
+    del document['_id']
     return document
+
+####################################################################################################################################
 async def fetch_all_users():
     users = []
     cursor = db.users.find({})
@@ -47,24 +102,60 @@ async def fetch_all_users():
         document['id'] = str(document['_id'])
         users.append(User(**document))
     return users
+
+
+####################################################################################################################################
 async def fetch_one_user(email):
     document = await db.users.find_one({"email": email})
     return document
 
+####################################################################################################################################
 async def update_user(email, name, password):
     await db.users.update_one({"email": email}, {"$set": {"name": name, "password": password}})
     document = await db.users.find_one({"email": email})
     return document
 
+####################################################################################################################################
 async def remove_user(email):
     await db.users.delete_one({"email": email})
     return True
 
+####################################################################################################################################
+async def fetch_one_organization(email):
+    document = await db.organizations.find_one({"email": email})
+    return document
+
+
+#####################################################################################################################################
+async def fetch_all_organizations():
+    organizations = []
+    cursor = db.organizations.find({})
+    async for document in cursor:
+        document['id'] = str(document['_id'])
+        organizations.append(Organization(**document))
+    return organizations
+
+#####################################################################################################################################
+async def create_organization(organization):
+    document = organization
+    organization_exists = await fetch_one_organization(document['email'])
+    if organization_exists:
+        raise HTTPException(status_code=400, detail="Organization already exists")
+    document['password'] = pwd_context.hash(document['password'])
+    result = await db.organizations.insert_one(document)
+    document['id'] = str(document['_id'])
+    del document['_id']
+    return document
+
+
+######################################################################################################################################
 # user routes 
 @app.get('/api/users/')
 async def get_users():
     response = await fetch_all_users()
     return response
+
+######################################################################################################################################
 @app.get('/api/users/{email}')
 async def get_user(email):
     response = await fetch_one_user(email)
@@ -72,28 +163,36 @@ async def get_user(email):
         return response
     raise HTTPException(404, f"There is no user with the email {email}")
 
+
+######################################################################################################################################
 @app.put('/api/users/{email}')
 async def put_user(email: str, name: str, password: str):
     response = await update_user(email, name, password)
     if response:
         return response
     raise HTTPException(404, f"There is no user with the email {email}")
-@app.post("/api/register/", response_model=User)
-async def register_user(user: User):
+
+#####################################################################################################################################
+@app.post("/api/register/user", response_model=User)
+async def register_user(user: NewUser):
     response = await create_user(user.dict())
-    if response:
+    if response:# org routes
         return response
     raise HTTPException(400, "Something went wrong")
-@app.post("/api/login/", response_model=User)
+
+#####################################################################################################################################
+@app.post("/api/login/user", response_model=User)
 async def login_user(email,password):
     response = await fetch_one_user(email)
     if response:
-        if response['password'] == password:
-            response['id'] = str(response['_id'])
-            del response['_id']
-            return response
-        raise HTTPException(400, "Password is incorrect")
-    raise HTTPException(404, f"There is no user with the email {email}")
+      if pwd_context.verify(password, response['password']):
+        response['id'] = str(response['_id'])
+        del response['_id']
+        return response
+      raise HTTPException(400, "Password is incorrect")
+    raise HTTPException(404, f"There is no user with the email {email}") 
+
+#####################################################################################################################################
 @app.delete('/api/users/{email}')
 async def delete_user(email):
     response = await remove_user(email)
@@ -101,61 +200,35 @@ async def delete_user(email):
         return "Successfully deleted user"
     raise HTTPException(404, f"There is no user with the email {email}")
 
+######################################################################################################################################
+# org db functions 
+######################################################################################################################################
+
+@app.post('/api/register/organization', response_model=Organization)
+def register_organization(organization: Organization):
+    response = create_organization(organization.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong")
+@app.post('/api/login/organization', response_model=Organization)
+def login_organization(email,password):
+    response = fetch_one_organization(email)
+    if response:
+      if pwd_context.verify(password, response['password']):
+        response['id'] = str(response['_id'])
+        del response['_id']
+        return response
+      raise HTTPException(400, "Password is incorrect")
+    raise HTTPException(404, f"There is no organization with the email {email}")
+
+#####################################################################################################################################
 # skills extract 
-import docx2txt
-import nltk
-import requests
-import docx
-nltk.download('punkt')
- 
- 
-def extract_text_from_docx(docx_path):
-    txt = docx2txt.process(docx_path)
-    if txt:
-        return txt.replace('\t', ' ')
-    return None
- 
- 
-def skill_exists(skill):
-    url = f'https://api.apilayer.com/skills?q={skill}&amp;count=1'
-    headers = {'apikey': 'tegtTot9eJxbtD7lxSoZPnGgR08dYYzo'}
-    response = requests.request('GET', url, headers=headers)
-    result = response.json()
- 
-    if response.status_code == 200:
-        return len(result) > 0 and result[0].lower() == skill.lower()
-    raise Exception(result.get('message'))
- 
- 
-def extract_skills(input_text):
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    word_tokens = nltk.tokenize.word_tokenize(input_text)
-
-    filtered_tokens = [w for w in word_tokens if w not in stop_words]
-
-    filtered_tokens = [w for w in word_tokens if w.isalpha()]
-
-    bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens, 2, 3)))
- 
-    found_skills = set()
-
-    for token in filtered_tokens:
-        if skill_exists(token.lower()):
-            found_skills.add(token)
- 
-    for ngram in bigrams_trigrams:
-        if skill_exists(ngram.lower()):
-            found_skills.add(ngram)
- 
-    return found_skills
-
+#####################################################################################################################################
 @app.get("/api/skills")
 async def get_skills():
-    text = extract_text_from_docx("/home/cyrus/Desktop/TeamCopium_JobFindr/app/server/DivyanshuKaushik_Resume.docx")
-    print(text)
-    # text = docx.Document("/home/cyrus/Desktop/TeamCopium_JobFindr/app/server/cc.docx")
-    skills = extract_skills(text)
- 
-    print(skills)
-    # print()
+    data = ResumeParser('/home/cyrus/Desktop/TeamCopium_JobFindr/app/server/DivyanshuKaushik_Resume.docx').get_extracted_data()
+    print(data['skills'])
     return True
+
+
+####################################################################################################################################
